8c95232bee63240cbcae6537d929c69ad479bca9
Alaa Yousfi <alaaelusfy@gmail.com>
2026-07-20 21:53:02 +0100

    Implement exit

--- app/main.go
+++ app/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
+
+func exit() {
+	os.Exit(-1)
+}
 
 func main() {
 	var command string
@@ -13,6 +18,9 @@ func main() {
 	for {
 		fmt.Print("$ ")
 		fmt.Scanf("%s", &command)
+		if command == "exit" {
+			exit()
+		}
 		fmt.Printf("%s: command not found\n", command)
 	}
 }
